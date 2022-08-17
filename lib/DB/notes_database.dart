02bab52315b9c7650e71383630bc9a63642f7190
zainahmed0597 <zainahmed0597@gmail.com>
2022-08-17 15:45:10 +0500

    first commit

---
+++ lib/DB/notes_database.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../model/note.dart';
+
+class NotesDatabase {
+  static final NotesDatabase instance = NotesDatabase._init();
+
+  static Database? _database;
+  NotesDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('notes.db');
+    return _database!;
+  }
+
+  // Open DB
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  // Create DB
+  Future _createDB(Database db, int version) async {
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final textType = 'TEXT NOT NULL';
+    final boolType = 'BOOLEAN NOT NULL';
+    final integerType = 'INTEGER NOT NULL';
+    await db.execute('''
+CREATE TABLE $tableNotes ( 
+  ${NoteFields.id} $idType, 
+  ${NoteFields.isImportant} $boolType,
+  ${NoteFields.number} $integerType,
+  ${NoteFields.title} $textType,
+  ${NoteFields.description} $textType,
+  ${NoteFields.time} $textType
+  )
+''');
+  }
+
+  // Close DB
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
